176c2c55bc3f1c8a1614d28406cc1c2857ca5da0
Károly Török <torok.karoly.krisztian@gmail.com>
2024-09-25 16:10:52 +0200

    Basic typing test

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -1,1 +1,34 @@
-<div>Hello world</div>
+<script lang="ts">
+  // import { invoke } from "@tauri-apps/api/core";
+
+  let txt = "lorem ipsum";
+  let letters = txt.split("");
+  $: caret = 0;
+  $: finish = caret === letters.length;
+
+  // async function greet() {
+  //   greetMsg = await invoke("greet", { name });
+  // }
+  //
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === letters[caret]) {
+      caret++;
+    }
+  };
+
+  const restart = () => {
+    caret = 0;
+  };
+</script>
+
+<div>
+  {#each letters as letter, i}
+    <letter style="opacity: {caret > i ? 1 : 0.25}">{letter}</letter>
+  {/each}
+  <p>{finish ? "🎉" : ""}</p>
+  {#if finish}
+    <button on:click={restart}>Restart</button>
+  {/if}
+</div>
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
