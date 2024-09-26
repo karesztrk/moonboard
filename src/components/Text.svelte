9f2abb492995508b954f19980058a812d3a0b0f3
Károly Török <torok.karoly.krisztian@gmail.com>
2024-09-26 19:32:46 +0200

    Random quote

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   // import { invoke } from "@tauri-apps/api/core";
+  import quotes from "@/assets/quote.json";
 
-  let txt = "lorem ipsum";
+  let { text: txt, author } = quotes[Math.floor(Math.random() * quotes.length)];
   let letters = txt.split("");
   $: caret = 0;
   $: finish = caret === letters.length;
@@ -22,10 +23,19 @@
 </script>
 
 <div>
-  {#each letters as letter, i}
-    <letter style="opacity: {caret > i ? 1 : 0.25}">{letter}</letter>
-  {/each}
-  <p>{finish ? "🎉" : ""}</p>
+  <blockquote>
+    <p>
+      {#each letters as letter, i}
+        <letter style="opacity: {caret > i ? 1 : 0.25}">{letter}</letter>
+      {/each}
+      {#if finish}
+        <span>🎉</span>
+      {/if}
+    </p>
+  </blockquote>
+  <p>
+    —{author}
+  </p>
   {#if finish}
     <button on:click={restart}>Restart</button>
   {/if}
