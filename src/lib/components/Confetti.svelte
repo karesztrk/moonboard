9443eeedc2f9975f47bbf9362c27380c81065198
Károly Török <torok.karoly.krisztian@gmail.com>
2024-10-10 18:28:07 +0200

    Tauri v2 and Confetti lib changed

--- src/lib/components/Confetti.svelte
+++ src/lib/components/Confetti.svelte
@@ -1,39 +1,26 @@
-<script>
-  import confetti from "canvas-confetti";
-  import { onMount } from "svelte";
-
-  // Demo values used
-  var duration = 15 * 1000;
-  var animationEnd = Date.now() + duration;
-  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  var interval = setInterval(() => {
-    var timeLeft = animationEnd - Date.now();
-
-    if (timeLeft <= 0) {
-      return clearInterval(interval);
-    }
-
-    var particleCount = 50 * (timeLeft / duration);
-    confetti({
-      ...defaults,
-      particleCount,
-      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-    });
-    confetti({
-      ...defaults,
-      particleCount,
-      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-    });
-  }, 500);
-
-  onMount(() => {
-    return () => {
-      clearInterval(interval);
-    };
-  });
+<script lang="ts">
+  import Confetti from "svelte-confetti";
 </script>
+
+<div
+  style="
+ position: fixed;
+ top: -50px;
+ left: 0;
+ height: 100vh;
+ width: 100vw;
+ display: flex;
+ justify-content: center;
+ overflow: hidden;
+ pointer-events: none;"
+>
+  <Confetti
+    x={[-5, 5]}
+    y={[0, 0.1]}
+    delay={[500, 2000]}
+    infinite
+    duration={5000}
+    amount={300}
+    fallDistance="100vh"
+  />
+</div>
